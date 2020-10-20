442fc7d1b3070f162615256f9da8b4dfee41fc27
Snehal Wagh <snehalwagh2121@gmail.com>
2020-10-20 10:20:10 +0530

    changing the nav layout for mobile view

--- public/js/index.js
+++ public/js/index.js
@@ -4,24 +4,28 @@ const status=document.querySelector('.status');
 const loginOptions=document.querySelector('.loginOptions');
 const logoutOptions=document.querySelectorAll('.logoutOptions');
 
-toggleMenu.addEventListener('click',e=>{
-    toogleLinks.classList.toggle('toggle-active');
-});
-
-if(status.innerText === 'LOGIN'){
-    console.log('LOGIN STATUS')
-    logoutOptions.forEach(element => {
-        element.classList.add('show');
-    });
-    loginOptions.classList.remove('show');
-}else if(status.innerText ==='LOGOUT'){
-    console.log('LOGOUT STATUS')
-    logoutOptions.forEach(element => {
-        element.classList.remove('show');
-    });
-    loginOptions.classList.add('show');
+const checkStatus=()=>{
+    if(status.innerText === 'LOGIN'){
+        console.log('LOGIN STATUS')
+        logoutOptions.forEach(element => {
+            element.classList.add('show');
+        });
+        loginOptions.classList.remove('show');
+    }else if(status.innerText ==='LOGOUT'){
+        console.log('LOGOUT STATUS')
+        logoutOptions.forEach(element => {
+            element.classList.remove('show');
+        });
+        loginOptions.classList.add('show');
+    }
 }
 
+toggleMenu.addEventListener('click',e=>{
+    toogleLinks.classList.toggle('toggle-active');
+    checkStatus();
+});
+
+checkStatus();
 
 // status.addEventListener('click', e=>{
 //     console.log('target = '+status.parentNode.parentNode.children[0].innerText);
