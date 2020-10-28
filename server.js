169400813e7809037acf835dce292881ce0394ff
Snehal Wagh <svishnuwagh@manh.com>
2020-10-29 01:35:57 +0530

    font changes, server logo change

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 var router = express.Router();
 const port = process.env.PORT || 3000;
+const favicon = require('serve-favicon');
+const path=require('path');
+const publicDirectory=path.join(__dirname,'../public');
 
 module.exports = router ;
 var routes = require('./routes/index');
 
 const server=express();
 server.set('view engine', 'ejs');
+
+server.use(favicon('public/images/favicon.ico'));
+server.use(express.static(publicDirectory));
 
 server.use('/', routes);
 
